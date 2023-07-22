3d318a4c0f69250ca63efce480af05c9ccce22e4
Maksym Boiar <boiar.max@gmail.com>
2023-07-22 18:14:30 +0200

    change ctest to gtest

--- test/test_gesture_detection.cpp
+++ test/test_gesture_detection.cpp
@@ -2,11 +2,10 @@
 
 #include "gesture_detection.h"
 
-
 TEST(GestureDetectorTest, detect) {
-    GestureDetector detector = GestureDetector();
     std::string img_path = "test/data/test.png";
     cv::Mat input_img = cv::imread(img_path, cv::IMREAD_COLOR);
+    GestureDetector detector = GestureDetector("resources/models/resnet18.onnx");
     ClassifierOutput output = detector.classify(input_img);
     std::string class_name = "palm";
     ASSERT_GE(output.score, 0.5);
